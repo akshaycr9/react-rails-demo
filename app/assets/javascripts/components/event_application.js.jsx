83b58a159054adff362e672c9e52fce4fe45fe37
akshaycr9 <akshay.thadani@gmail.com>
2016-11-27 16:30:36 +0530

    Adding react app

---
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -0,0 +1,66 @@
+var EventApplication = React.createClass({
+	getInitialState: function() {
+		return { events: [] };
+	},
+	componentDidMount: function() {
+		this.getDataFromApi();
+	},
+	getDataFromApi: function() {
+		var self = this;
+		$.ajax({
+			url: '/api/events',
+			success: function(data){
+				self.setState({ events: data });
+			},
+			error: function(xhr, status, error){
+				alert("cannot get data from API", error);
+			}
+		});
+	},
+	handleSearch: function(events){
+		this.setState({ events: events });
+	},
+	handleAdd: function(event){
+		var events = this.state.events;
+		events.push(event);
+		this.setState({ events: events });
+	},
+	handleUpdateRecord: function(old_event, event){
+		var events = this.state.events.slice();
+		var index = events.indexOf(old_event);
+		events.splice(index, 1, event);
+		this.setState({ events: events });
+	},
+	handleDeleteRecord: function(event) {
+		var events = this.state.events.slice();
+		var index = events.indexOf(event);
+		events.splice(index, 1);
+		this.setState({ events: events });
+	},
+	render: function() {
+		return (
+			<div className="container">
+				<div className="jumbotron">
+					<h1>React Demo</h1>
+					<p>by Akshay Thadani</p>
+				</div>
+				<div className="row">
+					<div className="col-md-4">
+						<SearchForm handleSearch={this.handleSearch} />
+					</div>
+					<div className="col-md-8">
+						<NewForm handleAdd={this.handleAdd} />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-12">
+						<EventTable
+						events={this.state.events}
+						handleDeleteRecord={this.handleDeleteRecord}
+						handleUpdateRecord={this.handleUpdateRecord} />
+					</div>
+				</div>
+			</div>
+		)
+	}
+});
---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,47 @@
+module Api
+	class EventsController < ApplicationController
+		before_action :set_event, only: [:update, :destroy]
+
+		def index
+			render json: Event.all
+		end
+
+		def create
+			event = Event.new(event_params)
+			if event.save
+				render json: event
+			else
+				render nothing: true, status: :bad_request
+			end
+		end
+
+		def update
+			if @event.update(event_params)
+				render json: @event
+			else
+				render nothing: true, status: :unprocessable_entity
+			end
+		end
+		
+		def search
+			query = params[:query]
+			events = Event.where('name LIKE ? OR place LIKE ? OR description LIKE ?', "%#{query}%", "%#{query}%", "%#{query}%")
+			render json: events
+		end
+
+		def destroy
+			@event.destroy
+			head :no_content
+		end
+
+		private
+
+		def set_event
+			@event = Event.find(params[:id])
+		end
+		
+		def event_params
+			params.require(:event).permit(:name, :description, :event_date, :place)
+		end
+	end
+end
